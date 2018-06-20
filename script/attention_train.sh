61b8954ef9a4cd7319bb89d34a52496354f7943a
pandigreat <pandigreat@163.com>
2018-06-21 02:23:57 +0800

    attention train2

--- script/attention_train.sh
+++ script/attention_train.sh
@@ -2,17 +2,17 @@
 touch log/attention.log
 log='log/attention.log'
 
-python ./attention_train.py     --train_data ./data/train_resnet50.txt \
-                             --test_data ./data/test_resnet50.txt \
+python ./attention_train.py     --train_data ./data/train.txt \
+                             --test_data ./data/test.txt \
                              --model ./model/resnet50_iter_3000.pkl  \
                              --n_classes 17 \
                              --offset 1 \
                              --lr 0.1 \
                              --lr_step 500 \
                              --gamma 0.1 \
-                             --batch_size 16 \
-                             --test_batch_size 8 \
-                             --test_iters 200 \
+                             --batch_size 32 \
+                             --test_batch_size 32 \
+                             --test_iters 100 \
                              --log $log \
                              --save_iter 1000 \
                              --shuffle True \
